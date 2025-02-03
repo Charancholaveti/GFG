5a35e36ac62a1874a6b99b45cc01f7e0ea2d66b1
charancholaveti <161560764+Charancholaveti@users.noreply.github.com>
2025-02-03 18:52:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+
+            Solution ob = new Solution();
+            int ans = ob.editDistance(s1, s2);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int editDistance(String word1, String word2) {
+        // Code 
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        
+        for (int i = 0; i <= n1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n2; j++) {
+            dp[0][j] = j;
+        }
+        
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insertion = 1 + dp[i][j - 1];
+                    int deletion = 1 + dp[i - 1][j];
+                    int replacement = 1 + dp[i - 1][j - 1];
+                    dp[i][j] = Math.min(insertion, Math.min(deletion, replacement));
+                }
+            }
+        }
+        
+        return dp[n1][n2];
+    }
+}
